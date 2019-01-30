db472401f646adc5fd50de435b18419522d4eab1
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-30 22:01:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,10 +4,15 @@
 # or whitespace (' ').
 class EmailParser
   attr_accessor :email
+
   def initialize(emails)
     @email = emails
   end
-  
+
+  def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array
+  end
 
 
 end
